04dccc560e646293e1aa9f71a2d407c92ec00a37
Abid <achowdhury@isrt.ac.bd>
2023-05-16 09:41:14 +0600

    sesh oise

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -1,25 +1,38 @@
 package com.example.assignment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ViewPager2 vp;
+    TabLayout tabLayout;
     ArrayList<Fruit> fruitArrayList;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         vp = findViewById(R.id.pager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         fruitArrayList = new Util().getAllFruits();
 
         VPAdapter adapter = new VPAdapter(fruitArrayList, this);
         vp.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, vp, (tab, position) -> {
+            tab.setIcon(R.drawable.baseline_circle_24);
+        }).attach();
+
     }
 }
